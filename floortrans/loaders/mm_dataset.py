95efdda39796fe8e231a09c4259589966e0e3908
Rune Havnung Bakken <rune@mazemap.com>
2020-09-22 10:23:55 +0200

    Prefetch keys and fix some bugs

--- floortrans/loaders/mm_dataset.py
+++ floortrans/loaders/mm_dataset.py
@@ -9,15 +9,16 @@ from torch.utils.data import Dataset
 class MMDataset(Dataset):
     _lmdb_env = None
 
-    def __init__(self, db_path, dataset, augmentations):
+    def __init__(self, db_path, dataset, augmentations=None):
         if not MMDataset._lmdb_env:
             MMDataset._lmdb_env = lmdb.open(
-                db_path, readonly=True, max_readers=8, lock=False, readahead=True, meminit=False)
+                db_path, readonly=True, max_readers=8, lock=False, readahead=True, meminit=False, max_dbs=3)
         self.augmentations = augmentations
-        self.db = MMDataset._lmdb_env.open_db(dataset)
+        self.db = MMDataset._lmdb_env.open_db(dataset.encode('ascii'))
+        self.keys = self._load_keys()
 
     def __getitem__(self, index):
-        key = str(index).encode('ascii')
+        key = self.keys[index]
         with MMDataset._lmdb_env.begin(db=self.db, write=False) as txn:
             data = txn.get(key)
         sample = pickle.loads(data)
@@ -28,8 +29,14 @@ class MMDataset(Dataset):
         return sample
 
     def __len__(self):
+        return len(self.keys)
+
+    def _load_keys(self):
         with MMDataset._lmdb_env.begin(db=self.db, write=False) as txn:
-            return txn.stat()['entries']
+            cursor = txn.cursor()
+            cursor.first()
+            keys = [key for key in cursor.iternext(keys=True, values=False)]
+        return keys
 
     @staticmethod
     def _prepare_img_data(img_data):
